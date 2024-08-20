92c5fdac308327748f0ea7097948fd08312b7d27
Carlos Longarela <carlos@longarela.eu>
2024-08-20 15:47:37 +0200

    Added cooldown option

--- cloudflare_events.sh
+++ cloudflare_events.sh
@@ -4,6 +4,7 @@
 THRESHOLD=1000 # Number of firewall events to trigger the alert.
 INTERVAL=300   # Time interval between checks in seconds.
 LANGUAGE="EN"  # Language for alerts: EN (English) or ES (Spanish).
+COOLDOWN=21600 # Time to wait after an alert in seconds (6 hours by default).
 
 # include file with Cloudflare variables
 source CLOUDFLARE_CONFIG
@@ -16,6 +17,7 @@ show_help_en() {
     echo "Usage: $0 -t <event_threshold> -i <interval_seconds> [-l ES|EN] [-h] [-a]"
     echo "  -t: Number of firewall events to trigger the alert (must be greater than 100)."
     echo "  -i: Time interval between checks in seconds (must be greater than 60)."
+    echo "  -c: (Optional) Waiting period after an alert in seconds. Default is 21600 (6 hours)."
     echo "  -l: (Optional) Language of the alert message. Default is EN (English)."
     echo "  -h: Show this help message in English."
     echo "  -a: Show this help message in Spanish."
@@ -27,6 +29,7 @@ show_help_es() {
     echo "Uso: $0 -t <umbral_eventos> -i <intervalo_segundos> [-l ES|EN] [-h] [-a]"
     echo "  -t: Número de eventos de firewall para activar la alerta (debe ser mayor de 100)."
     echo "  -i: Intervalo de tiempo entre verificaciones en segundos (debe ser mayor de 60)."
+    echo "  -c: (Opcional) Período de espera después de una alerta en segundos. Por defecto 21600 (6 horas)."
     echo "  -l: (Opcional) Idioma del mensaje de alerta. Por defecto EN (Inglés)."
     echo "  -h: Mostrar esta ayuda en inglés."
     echo "  -a: Mostrar esta ayuda en español."
@@ -34,10 +37,11 @@ show_help_es() {
 }
 
 # Obtain the options we passed to the script.
-while getopts "t:i:l:h:a" opt; do
+while getopts "t:i:c:l:h:a" opt; do
     case "$opt" in
         t) THRESHOLD=$OPTARG ;;
         i) INTERVAL=$OPTARG ;;
+        c) COOLDOWN=$OPTARG ;;
         l) LANGUAGE=$OPTARG ;;
         h) show_help_en ;;
         a) show_help_es ;;
@@ -59,47 +63,54 @@ send_telegram_message() {
 }
 
 # Principal loop
+last_alert_time=0
 while true; do
-  # Actual date and 24 hours ago in ISO 8601 format.
-  DATE_NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-  DATE_24H_AGO=$(date -u -d "24 hours ago" +"%Y-%m-%dT%H:%M:%SZ")
+  current_time=$(date +%s)
 
-  # GraphQL query
-  read -r -d '' QUERY << EOF
-  {
-    viewer {
-      zones(filter: {accountTag: "${CF_ACCOUNT_ID}"}) {
-        firewallEventsAdaptiveGroups(
-          limit: 1,
-          filter: {
-            datetime_geq: "${DATE_24H_AGO}",
-            datetime_leq: "${DATE_NOW}"
-          }
-        ) {
-          count
+  # Check if the cooldown period has passed since the last alert.
+  if (( current_time - last_alert_time >= COOLDOWN )); then
+    # Actual date and 24 hours ago in ISO 8601 format.
+    DATE_NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    DATE_24H_AGO=$(date -u -d "24 hours ago" +"%Y-%m-%dT%H:%M:%SZ")
+
+    # GraphQL query
+    read -r -d '' QUERY << EOF
+    {
+        viewer {
+        zones(filter: {accountTag: "${CF_ACCOUNT_ID}"}) {
+            firewallEventsAdaptiveGroups(
+            limit: 1,
+            filter: {
+                datetime_geq: "${DATE_24H_AGO}",
+                datetime_leq: "${DATE_NOW}"
+            }
+            ) {
+            count
+            }
         }
-      }
+        }
     }
-  }
 EOF
 
-  # Make the request with curl.
-  response=$(curl -s -X POST "${CF_GRAPHQL_ENDPOINT}" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${CF_API_KEY}" \
-    --data "{\"query\":\"${QUERY}\"}")
+    # Make the request with curl.
+    response=$(curl -s -X POST "${CF_GRAPHQL_ENDPOINT}" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${CF_API_KEY}" \
+        --data "{\"query\":\"${QUERY}\"}")
 
-  # Extract the number of firewall events.
-  event_count=$(echo "$response" | jq '.data.viewer.zones[0].firewallEventsAdaptiveGroups[0].count')
+    # Extract the number of firewall events.
+    event_count=$(echo "$response" | jq '.data.viewer.zones[0].firewallEventsAdaptiveGroups[0].count')
 
-  # Verification and sending of alert.
-  if [[ "$event_count" -gt "$THRESHOLD" ]]; then
-    if [[ "$LANGUAGE" == "EN" ]]; then
-      message="⚠️ Alert (${CF_ACCOUNT_NAME}): ${event_count} firewall events have been detected in the last 24 hours, exceeding the threshold of ${THRESHOLD}."
-    else
-      message="⚠️ Alerta (${CF_ACCOUNT_NAME}): Se han detectado ${event_count} eventos de firewall en las últimas 24 horas, lo que supera el umbral de ${THRESHOLD}."
+    # Verification and sending of alert.
+    if [[ "$event_count" -gt "$THRESHOLD" ]]; then
+        if [[ "$LANGUAGE" == "EN" ]]; then
+        message="⚠️ Alert (${CF_ACCOUNT_NAME}): ${event_count} firewall events have been detected in the last 24 hours, exceeding the threshold of ${THRESHOLD}."
+        else
+        message="⚠️ Alerta (${CF_ACCOUNT_NAME}): Se han detectado ${event_count} eventos de firewall en las últimas 24 horas, lo que supera el umbral de ${THRESHOLD}."
+        fi
+        send_telegram_message "$message"
+        last_alert_time=$current_time
     fi
-    send_telegram_message "$message"
   fi
 
   # Wait before the next check.
